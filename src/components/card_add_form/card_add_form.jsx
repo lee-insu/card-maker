5fe2752b29090e6d3c62e65b5e2aed439a33ad7f
lee-insu <carpediem7760@gmail.com>
2021-05-11 21:22:07 +0900

    image

--- src/components/card_add_form/card_add_form.jsx
+++ src/components/card_add_form/card_add_form.jsx
@@ -1,9 +1,10 @@
 import React, {useRef} from 'react';
 import styles from './card_add_form.module.css';
 import Button from '../button/button';
-import ImageFileInput from '../image_file_input/image_file_input';
+import { useState } from 'react';
 
-const CardAddForm = ({ onAdd }) => {
+
+const CardAddForm = ({ FileInput, onAdd }) => {
 
   const formRef = useRef();
   const nameRef = useRef();
@@ -12,7 +13,16 @@ const CardAddForm = ({ onAdd }) => {
   const titleRef = useRef(); 
   const emailRef = useRef();
   const messageRef = useRef();
-
+  const [file,setFile] = useState({
+     fileName:null,
+     fileURL:null  
+  })
+  const onFileChange = file =>{
+    setFile({
+      fileName:file.name,
+      fileURL:file.url,
+    })
+  }
   const onSubmit = (event) => {
     event.preventDefault();
     const card = {
@@ -23,10 +33,14 @@ const CardAddForm = ({ onAdd }) => {
       title:titleRef.current.value || '',
       email:emailRef.current.value || '',
       message:messageRef.current.value || '',
-      fileName:'',
-      fileURL:''
+      fileName:file.fileName ||'',
+      fileURL:file.fileURL||''
     };
     formRef.current.reset();
+    setFile({
+      fileName:null,
+      fileURL:null 
+    });
     onAdd(card)
 
   };
@@ -86,7 +100,7 @@ const CardAddForm = ({ onAdd }) => {
       />
 
       <div className={styles.fileInput}>
-        <ImageFileInput />
+        <FileInput name={file.fileName} onFileChange = {onFileChange}/>
       </div>
       <Button name="add" onClick={onSubmit} />
     </form>
--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './card_edit_form.module.css';
 import Button from '../button/button';
-import ImageFileInput from '../image_file_input/image_file_input';
 
-const CardEditForm = ({ card,updateCard,deleteCard }) => {
+
+const CardEditForm = ({ FileInput,card,updateCard,deleteCard }) => {
   const {
     name,
     company,
@@ -14,6 +14,15 @@ const CardEditForm = ({ card,updateCard,deleteCard }) => {
     fileName,
     fileURL, 
   } = card;
+
+  const onFileChange= file => {
+    updateCard({
+      ...card,
+      fileName:file.name,
+      fileURL:file.url,
+    })
+  }
+
 
   const onChange = (event) => {
     if (event.currentTarget == null) {
@@ -74,9 +83,9 @@ const CardEditForm = ({ card,updateCard,deleteCard }) => {
 
 
       <div className={styles.fileInput}>
-        <ImageFileInput />
+        <FileInput  onFileChange = {onFileChange} />
       </div>
-      <Button name="Delete" onClick={onSubmit} />
+    <Button name="Delete" onClick={onSubmit}  />
     </form>
   );
 };
